2b3d5bbf183385880c367b1cb930574fcde66959
sailaja1323 <80672899+sailaja1323@users.noreply.github.com>
2021-03-17 16:21:19 +0530

    uploaded my project

---
+++ hello_maven/src/test/java/hello_maven/AppTest.java
@@ -0,0 +1,14 @@
+package hello_maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
